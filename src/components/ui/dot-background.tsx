31eee700798eb2a8757383e00db02de19047f11b
Joern Depenbrock <joern@j-d-pictures.de>
2025-09-03 14:52:19 +0200

    - upated dot background to keep colors in sync with figma & widgetbook

--- src/components/ui/dot-background.tsx
+++ src/components/ui/dot-background.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { cn } from "@/lib/utils"
 
 type DotBackgroundProps = {
@@ -7,16 +6,15 @@ type DotBackgroundProps = {
 
 export function DotBackground({ className }: DotBackgroundProps) {
   return (
-    <div className={cn("pointer-events-none absolute inset-0", className)}>
+    <div className={cn("pointer-events-none absolute inset-0 bg-[#2B313B]", className)}>
       <div
         className={cn(
           "absolute inset-0",
-          "[background-size:20px_20px]",
-          "[background-image:radial-gradient(#1f1f1f_1px,transparent_1px)]",
-          "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+          "[background-size:11px_11px]",
+          "[background-image:radial-gradient(#303440_1px,transparent_1px)]"
         )}
       />
-      <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent_0px,black_300px,black_100%)]" />
     </div>
   )
 }
