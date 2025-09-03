ec8faf50a12878e3f26e4bf5c4d0c442de20672e
Joern Depenbrock <joern@j-d-pictures.de>
2025-09-03 15:02:36 +0200

    - increased dotted background dot size

--- src/components/ui/dot-background.tsx
+++ src/components/ui/dot-background.tsx
@@ -11,7 +11,7 @@ export function DotBackground({ className }: DotBackgroundProps) {
         className={cn(
           "absolute inset-0",
           "[background-size:11px_11px]",
-          "[background-image:radial-gradient(#303440_1px,transparent_1px)]"
+          "[background-image:radial-gradient(#303440_2px,transparent_2px)]"
         )}
       />
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent_0px,black_300px,black_100%)]" />
