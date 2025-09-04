330604dd1b0341db65d1066a9c6c7733ca63c162
Joern Depenbrock <joern@j-d-pictures.de>
2025-09-04 16:30:44 +0200

    - correct base path to align with github

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/unping-ui-website/',
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "./src"),
